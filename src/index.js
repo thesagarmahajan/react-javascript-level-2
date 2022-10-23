0e586b1164ef39b3a80110cca090be6769dee6b1
thesagarmahajan <sagarmahajan31997@gmail.com>
2022-10-23 10:31:02 +0530

    Normalize App and Index files

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AllUsers2 from './users/AllUsers2';
-// import AddUser from './users/AddUser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <AllUsers2 />
-  </React.StrictMode>
+  // <React.StrictMode>
+      <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
